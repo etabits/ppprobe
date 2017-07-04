1eb49dad743df954ed4d35236eed33ede281ab9c
Hasan Arous <arous.hasan@gmail.com>
2017-07-04 13:40:25 +0300

    telnet support

--- Connection.js
+++ Connection.js
@@ -1,5 +1,7 @@
 'use strict'
-var node_ssh = require('node-ssh')
+const node_ssh = require('node-ssh')
+const telnet = require('telnet-client')
+
 class Connection {
   constructor (opts) {
     this.opts = opts
@@ -11,6 +13,14 @@ class Connection {
       this.conn = new node_ssh()
       return this.conn.connect(this.opts)
       .then(()=>this)
+    } else if ('telnet'==this.type) {
+      this.conn = new telnet();
+      return this.conn.connect(Object.assign({
+        timeout: 30e3,
+        sendTimeout: 30e3,
+        execTimeout: 30e3,
+        port: 23,
+      }, this.opts)).then(()=>this)
     }
   }
 
@@ -21,6 +31,8 @@ class Connection {
         // console.error(res)
         return res.stdout
       })
+    } else if ('telnet'==this.type) {
+      return this.conn.exec(cmd)
     }
   }
 }
